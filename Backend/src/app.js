43502599239b097ac42a27516987cb7b481986f0
Mohd Amaan <mohdamaan@Mohds-MacBook-Air.local>
2026-09-10 12:30:07 +0530

    cors origin updated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     // origin: "http://localhost:5173", For local development
-    origin: "https://interview-prep-ai.vercel.app",
+    origin: "https://interview-prep-ai-chi-ten.vercel.app/",
     credentials: true
 }))
 
